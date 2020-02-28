858c677483f23894e8f9be4a5e6367441afe8889
James Ross <jross.nyc111@gmail.com>
2020-02-28 00:07:07 -0500

    :fire:

--- front-end/src/components/Transaction.js
+++ front-end/src/components/Transaction.js
@@ -4,11 +4,13 @@ export const Transaction = ({ transaction }) => {
 	const sign = transaction.amount < 0 ? '-' : '+';
 	return (
 		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-			{transaction.text}{' '}
+			<button className='delete-btn'>X</button>
+			{'  '}
+			{transaction.text}
+			{'              '}
 			<span>
 				{sign}${Math.abs(transaction.amount)}
 			</span>{' '}
-			<button className='delete-btn'>X</button>
 		</li>
 	);
 };
