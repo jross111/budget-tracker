f311e40367830de8baf775a9c9dd575f8df38b35
James Ross <jross.nyc111@gmail.com>
2020-03-20 10:36:12 -0400

    :rocket:

--- scratch.js
+++ scratch.js
@@ -1,7 +1,11 @@
 function squareSum(numbers) {
 	numbers.forEach((el) => {
 		let sum = 0;
-		sum += el * el;
+		sum = sum + el * el;
 		console.log(sum);
+		let arrSum = (sum) => sum.reduce((a, b) => a + b, 0);
+		console.log(arrSum);
 	});
 }
+
+squareSum([1, 2, 3]);
